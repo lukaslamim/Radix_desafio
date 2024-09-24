b27c5a5fbe7af0e76fb417ed5163ef6ca4bd0107
lucas lamim <lukas.lamim@hotmail.com>
2024-09-24 17:53:50 -0300

    #config
    #fist api route

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
-from BD import sensors
+from flask import Flask, Request, Response, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lukas:741852@localhost:5432/sensor_database'
 
-@app.route('/sensor', methods=['GET'])
-def getSensorData():
-    return sensors
+db = SQLAlchemy(app)
+
+#create sensor model
+class sensor(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  
+    equipmentid = db.Column(db.String(10), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)  
+    value = db.Column(db.Numeric, nullable=False)  
+    
+    def toJson(self):
+        return {
+            "id": self.id,
+            "equipmentId": self.equipmentid,
+            "timestamp": self.timestamp.isoformat(),  
+            "value": float(self.value)  
+        }
+        
+def listToJson(List):
+    return [object.toJson() for object in List]
+        
+def createResponde(content):
+    return make_response(
+        jsonify(
+        content
+        )
+    )    
+
+#select all
+@app.route('/sensors', methods = ['GET'])
+def SelectAll():
+    return createResponde(listToJson(sensor.query.all()))
+
+#select by id
+@app.route('/sensor/id', methods = ['GET'])
+def SelectById(id):
+    return createResponde(listToJson(sensor.query))
 
 app.run()
